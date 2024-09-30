570e47732537056ab5be6d80e5a1a167c609ed74
Hong-Ng <162881813+Hong-Ng@users.noreply.github.com>
2024-10-01 04:12:21 +0800

    2.1 done, 2.2 halfway there
    ~ 2.1 ~
    I made a function to count the number of characters in a name, using that function to count the number of characters of a name on the list, before comparing that against the highest and lowest character count. If it beats one of them, the number of characters becomes the new character count and the name is saved. Once the code has gone through all the names, it prints out the longest name and the shortest name as well as the total number of names.
    
    ~ 2.2 ~
    I asked for the input of a name. I then used the character counter to count the number of characters in the name before subtracting to account for the fact that character count starts from 0 and not 1. A loop is then created where as long as the second character count is lower or the same as the total number of characters, it keeps going. I have 2 variables, with 'a' starting at 0 (the first letter) and 'b' starting at 2 (the second letter). A list that stores the pairs has been created and the variables are appended to the list as a pair. I then add 1 to each letter everytime the code loops so it slowly moves forward letter until the loop breaks. The resulting list of pairs is then printed. I still have the second part of 2.2 to do, I may skip it and leave it till later

--- main.py
+++ main.py
@@ -1,10 +1,59 @@
-
+min = 5
+max = 5
 names = []
-
+namequantity = 0
 
 with open('names.txt', 'r') as file:
     names = file.readlines()
     names = [name.strip() for name in names]
-    
 
-print (names)
+def namelength(name):
+    a = 0
+    for char in name:
+        a= a + 1
+    return a
+def minmaxnames(names, min, max):
+    for name in names:
+        a = 0
+        a = namelength(name)
+        namequantity = 0
+        namequantity = namequantity + 1
+        if a >= max:
+            maxname = name
+            max = a
+        elif a <= min:
+            minname = name
+            min = a
+    print("Number of names:", namequantity)    
+    print("Longest name:", maxname + ". Shortest name:", minname)
+
+def pairing():
+    num = 0
+    pairlist = []
+    a = 0
+    b = 1   
+    name = input("Enter name: ")
+    for char in name:
+        num = num+1
+    num = num - 1
+    while b <= num:
+        
+        pair = name[a], name[b]
+        pairlist.append(pair)
+        a = a+1
+        b = b+1
+    print (pairlist)
+    
+while True:   
+    print("Welcome to Hell")
+    print("Choose an option:")
+    print("1. Find the Longest and Shortest names")
+    print("2. Enter a name and recieve the character pairings")
+    print("TO QUIT ENTER 0")
+    inputs = int(input())
+    if inputs == 1:
+        minmaxnames(names, min, max)
+    elif inputs == 2:
+        pairing()
+    elif inputs == 0:
+        break
