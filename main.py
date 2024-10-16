293a5035b86bc11f630afcad7548cfced9b2fa06
Hong-Ng <162881813+Hong-Ng@users.noreply.github.com>
2024-10-16 23:33:56 +0800

    Done half of 3.2

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ names = []
 namequantity = 0
 freqlist = []
 namefrequency = {}
+keyfrequency = {}
 x = 0
-#opening the .txt file before taking all the names out as a list
+#oOpening the .txt file before taking all the names out as a list
 with open('names.txt', 'r') as file:
     names = file.readlines()
     names = [name.strip() for name in names]
@@ -23,15 +24,15 @@ def minmaxnames(names, min, max):
         a = namelength(name)
         namequantity = 0
         namequantity = namequantity + 1
-        #counter for the names
+#Counter for the names
         if a >= max:
             maxname = name
             max = a
-            #if the length of the name exceeded the current best, then it would become the new name, else nothing would happen
+#If the length of the name exceeded the current best, then it would become the new name, else nothing would happen
         elif a <= min:
             minname = name
             min = a
-            #same here if the length of the name was shorter than the current best
+#Same here if the length of the name was shorter than the current best
     print("Number of names:", namequantity)    
     print("Longest name:", maxname + ". Shortest name:", minname)
 
@@ -42,26 +43,33 @@ def pairing(name):
     b = 1   
     num = namelength(name)
     num = num - 1
+#I take the first letter and add a hash onto it to make it easy to identify if the letter started a word later on
     pair = "#", name[a]
     pairlist.append(pair)
     while b <= num:
         pair = name[a], name[b]
         pairlist.append(pair)
+#Pairing the names together before adding them to a larger list
         a = a+1
         b = b+1
     pair = name[b-1], "$"
+#Added a dollar sign to the last character to make it easier to tell which characters end names
     pairlist.append(pair)
     return (pairlist)
+#Returning the word divided into pairs
         
 def cointoss():
     coin = ['0','1']
+#Outcomes, with 0 being tails and 1 being heads
     coin_probs = [0.8,0.2]
+#Coin probablities, 0.8 and 0.2 being 0 and 1 respectively
     return r.choices(coin,coin_probs)
 
 def wheelspin():
     spinner = ['0','1','2','3']
     spinner_probs = [0.2,0.1,0.1,0.6]
     return r.choices(spinner,spinner_probs)
+#Same as the coin toss
 
 while True:   
     print("Choose an option:")
@@ -71,6 +79,7 @@ while True:
     print("4. Weighted Coin Toss")
     print("5. Wheel Spin")
     print("TO QUIT ENTER 0")
+#Menu
     inputs = int(input())
     if inputs == 1:
         minmaxnames(names, min, max)
@@ -88,6 +97,15 @@ while True:
         with open('pair_freq_sorted.txt', 'w') as file:
             for key, value in namefrequency.items():
                 file.write(f"{key}: {value}\n")
+        for key in namefrequency:
+            print(key[0])
+            if key[0] in keyfrequency:
+                keyfrequency[key[0]] = namefrequency[key] + keyfrequency[key[0]]
+            else:
+                keyfrequency[key[0]] = namefrequency[key]
+        with open('Starting_Character_Freq.txt', 'w') as file:
+            for key, value in keyfrequency.items():
+                file.write(f"{key}: {value}\n")
     elif inputs == 4:
         i = 1
         while i < 21:
@@ -98,5 +116,7 @@ while True:
         while i < 21:
             print(wheelspin())
             i = i + 1
+#If the user enters a number on the menu it will them excute the function accordingly
     elif inputs == 0:
         break
+#If the user enters 0 the loop is broken and the code ends
